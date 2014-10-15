2b44f63b20e9272bd1258847f043b476c3ceb3f0
Gust <gust@argon.local>
2014-10-15 14:11:10 -0400

    wip

--- src/Main.hs
+++ src/Main.hs
@@ -2,21 +2,44 @@ module Main where
 
 import           Control.Concurrent (MVar (..), forkIO, newEmptyMVar, putMVar,
                                      takeMVar)
-import           GHC.IO.Handle      (hGetContents)
+import           System.IO          (hGetContents, hPutStrLn)
 import           System.Process     (CreateProcess (..), StdStream (..),
                                      createProcess, proc)
 
 main = do
-  mvar <- newEmptyMVar
-  forkIO $ ls mvar
-  s <- takeMVar mvar
-  putStrLn $ "got: " ++ s
+  recordToValidate <- newEmptyMVar
+  validationResult <- newEmptyMVar
+  forkIO $ getRecordsToValidate recordToValidate
+  forkIO $ validationWorker recordToValidate validationResult
+  loop validationResult
+    where
+      loop validationResult = do
+        s <- takeMVar validationResult
+        putStrLn $ "result: " ++ s
+        loop validationResult
 
 
-ls :: MVar String -> IO ()
-ls mvar = do
-  (_, Just hout, _, _) <- createProcess (proc "ls" []){ std_out = CreatePipe }
+
+getRecordsToValidateProc = proc "rake" ["db:validate:perform"]
+validationWorkerProc     = proc "rake" ["db:validate:worker"]
+
+getRecordsToValidate :: MVar String -> IO ()
+getRecordsToValidate recordToValidate = do
+  (_, Just hout, _, _) <- createProcess getRecordsToValidateProc{ std_out = CreatePipe }
   s <- hGetContents hout
-  putMVar mvar s
+  mapM_ (putMVar recordToValidate) $ lines s
 
+validationWorker :: MVar String -> MVar String -> IO ()
+validationWorker recordToValidate validationResult = do
+  (Just hin, Just hout, _, _) <- createProcess validationWorkerProc{ std_in = CreatePipe, std_out = CreatePipe }
+  loop hin hout recordToValidate validationResult
+    where
+      loop hin hout recordToValidate validationResult = do
+        validationRequest <- takeMVar recordToValidate
+        putStrLn $ "worker received directive: " ++ validationRequest
+        hPutStrLn hin validationRequest
+        validationResponse <- hGetContents hout
+        putStrLn $ "worker responded with: " ++ validationResponse
+        putMVar validationResult validationResponse
+        loop hin hout recordToValidate validationResult
 
